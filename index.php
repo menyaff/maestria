8223744fd41fc0b1d961578bf2232edac80139ef
Manuel Adrián Flores <Manuel Adrin Flores>
2016-07-11 23:16:44 -0500

    Fix de URL

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
-		<form action="http://www.google.com/search/" method="get">
+		<form action="http://www.google.com/search" method="get">
 			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png" width="30" />
 			&nbsp;
 			<input type="text" name="q" placeholder="Buscar">
