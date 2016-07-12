4130d418d9cde83f0101f42b6fff8c25045dcc23
Manuel Adrián Flores <Manuel Adrin Flores>
2016-07-11 23:08:41 -0500

    Se agrega index de buscar en google

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tarea 2</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<form action="http://www.google.com/search/" method="get">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png" width="30" />
+			&nbsp;
+			<input type="text" name="q" placeholder="Buscar">
+			&nbsp;
+			<button type="submit">Aceptar</button>
+		</form>
+	</body>
+</html>
